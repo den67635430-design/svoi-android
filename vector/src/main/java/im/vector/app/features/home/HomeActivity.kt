73d7be1996286f4b9fff685461925c3386de549a
den67635430-design <den67635430@gmail.com>
2026-04-24 17:50:31 +0300

    feat: call SvoiVersionChecker.checkForUpdates in HomeActivity.onResume

--- vector/src/main/java/im/vector/app/features/home/HomeActivity.kt
+++ vector/src/main/java/im/vector/app/features/home/HomeActivity.kt
@@ -77,6 +77,8 @@ import im.vector.app.features.spaces.share.ShareSpaceBottomSheet
 import im.vector.app.features.themes.ThemeUtils
 import im.vector.app.features.usercode.UserCodeActivity
 import im.vector.app.features.workers.signout.ServerBackupStatusViewModel
+import im.vector.app.features.svoi.api.SvoiApiClient
+import im.vector.app.features.svoi.updates.SvoiVersionChecker
 import im.vector.lib.core.utils.compat.getParcelableExtraCompat
 import im.vector.lib.strings.CommonStrings
 import kotlinx.coroutines.Dispatchers
@@ -199,6 +201,9 @@ class HomeActivity :
         get() = views.coordinatorLayout
 
     override fun getBinding() = ActivityHomeBinding.inflate(layoutInflater)
+
+    @Inject lateinit var svoiApiClient: SvoiApiClient
+    private var svoiVersionChecker: SvoiVersionChecker? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -586,6 +591,12 @@ class HomeActivity :
 
     override fun onResume() {
         super.onResume()
+
+        // SVOI: проверка обновлений (throttled, не блокирует UI)
+        if (svoiVersionChecker == null) {
+            svoiVersionChecker = SvoiVersionChecker(this, svoiApiClient)
+        }
+        svoiVersionChecker?.checkForUpdates(lifecycleScope)
 
         if (vectorUncaughtExceptionHandler.didAppCrash()) {
             vectorUncaughtExceptionHandler.clearAppCrashStatus()
