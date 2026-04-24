62ad8f2d6e457ea27c11b274f93002b1573fb5f5
den67635430-design <den67635430@gmail.com>
2026-04-24 08:40:47 +0300

    fix: revert broken comment insertion in selectMatrixOrg

--- vector/src/main/java/im/vector/app/features/login/LoginServerSelectionFragment.kt
+++ vector/src/main/java/im/vector/app/features/login/LoginServerSelectionFragment.kt
@@ -37,7 +37,7 @@ class LoginServerSelectionFragment :
 
     private fun initViews() {
         views.loginServerChoiceEmsLearnMore.debouncedClicks { learnMore() }
-        views.loginServerChoiceMatrixOrg.debouncedClicks { selectMatrixOrg() // Now points to svo.kodkontenta.ru // Now points to svo.kodkontenta.ru }
+        views.loginServerChoiceMatrixOrg.debouncedClicks { selectMatrixOrg() }
         views.loginServerChoiceEms.debouncedClicks { selectEMS() }
         views.loginServerChoiceOther.debouncedClicks { selectOther() }
         views.loginServerIKnowMyIdSubmit.debouncedClicks { loginWithMatrixId() }
@@ -58,7 +58,7 @@ class LoginServerSelectionFragment :
         openUrlInChromeCustomTab(requireActivity(), null, EMS_LINK)
     }
 
-    private fun selectMatrixOrg() // Now points to svo.kodkontenta.ru // Now points to svo.kodkontenta.ru {
+    private fun selectMatrixOrg() {
         loginViewModel.handle(LoginAction.UpdateServerType(ServerType.MatrixOrg))
     }
 
